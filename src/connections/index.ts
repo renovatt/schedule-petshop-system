c63e782fac53efaab916b972381e6b7be8b3bc31
Will <wlymes@gmail.com>
2023-02-21 11:48:10 -0300

    feat: schedule daily count function added

--- src/connections/index.ts
+++ src/connections/index.ts
@@ -3,8 +3,17 @@ import { DataListSchedulesProps } from "@/components/Forms/ScheduleForm/types";
 export async function getSchedulesMonthQuantity(schedules: DataListSchedulesProps): Promise<number> {
     const currentMonth = new Date().getMonth() + 1;
     const currentMonthSchedules = schedules?.schedules?.filter(schedule => {
-        const scheduleDate = new Date(schedule.date);
-        return scheduleDate.getMonth() + 1 === currentMonth;
+        const scheduleDate = new Date(schedule.date).getMonth() + 1;
+        return scheduleDate === currentMonth;
     });
     return currentMonthSchedules?.length ?? 0;
+}
+
+export async function getSchedulesDailyQuantity(schedules: DataListSchedulesProps): Promise<number> {
+    const currentDay = new Date().getDate();
+    const currentDaySchedules = schedules?.schedules?.filter(schedule => {
+        const scheduleDate = new Date(schedule.date).getDate();
+        return scheduleDate === currentDay;
+    });
+    return currentDaySchedules?.length ?? 0;
 }
