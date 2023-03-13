888c8e4fe7a382a656796f3e2453e43e46ae7ec2
Will <wlymes@gmail.com>
2023-03-13 12:08:00 -0300

    fix: navigation issue fixed

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import * as S from './style'
-import { parseCookies } from 'nookies';
-import Navigation from '../Navigation'
 
 export type LayoutProps = {
     children: React.ReactNode;
 };
 
 const Layout = ({ children }: LayoutProps) => {
-    const { ['@nextauth-token']: token } = parseCookies();
     return (
         <S.Container>
-            {token && <Navigation />}
             <S.Main>{children}</S.Main>
         </S.Container>
     )
