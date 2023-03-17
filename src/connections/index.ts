fd6e1941ca117c5d806d72a3e91a038c1630e41e
Will <wlymes@gmail.com>
2023-03-17 11:44:38 -0300

    refactor: refactored functions

--- src/connections/index.ts
+++ src/connections/index.ts
@@ -1,19 +1,36 @@
 import { DataListSchedulesProps } from "@/components/Forms/ScheduleForm/types";
 
-export async function getSchedulesMonthQuantity(schedules: DataListSchedulesProps): Promise<number> {
-    const currentMonth = new Date().getMonth() + 1;
-    const currentMonthSchedules = schedules?.schedules?.filter(schedule => {
-        const scheduleDate = new Date(schedule.date).getMonth() + 1;
-        return scheduleDate === currentMonth;
+function countSchedules(
+    schedules: DataListSchedulesProps,
+    getDate: (schedule: any) => number,
+    predicate: (schedule: any) => boolean
+): number {
+    const currentDate = getDate(new Date());
+    const currentSchedules = schedules?.schedules?.filter(schedule => {
+        const scheduleDate = getDate(new Date(schedule.date));
+        return scheduleDate === currentDate && predicate(schedule);
     });
-    return currentMonthSchedules?.length ?? 0;
+    return currentSchedules?.length ?? 0;
+}
+
+export async function getSchedulesMonthQuantity(schedules: DataListSchedulesProps): Promise<number> {
+    return countSchedules(
+        schedules,
+        date => date.getMonth() + 1,
+        schedule => new Date(schedule.date)
+            .getMonth() + 1 === new Date().getMonth() + 1 && schedule.status === true
+    );
+}
+
+export async function getSchedulesMonthCanceledQuantity(schedules: DataListSchedulesProps): Promise<number> {
+    return countSchedules(
+        schedules,
+        date => date.getMonth() + 1,
+        schedule => new Date(schedule.canceled_date)
+            .getMonth() + 1 === new Date().getMonth() + 1 && schedule.status === false
+    );
 }
 
 export async function getSchedulesDailyQuantity(schedules: DataListSchedulesProps): Promise<number> {
-    const currentDay = new Date().getDate();
-    const currentDaySchedules = schedules?.schedules?.filter(schedule => {
-        const scheduleDate = new Date(schedule.date).getDate();
-        return scheduleDate === currentDay;
-    });
-    return currentDaySchedules?.length ?? 0;
+    return countSchedules(schedules, date => date.getDate(), schedule => schedule.status === true);
 }
