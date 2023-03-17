01e56f37fdd1d58befc315608939709bdfb2d8dc
Will <wlymes@gmail.com>
2023-03-17 11:47:11 -0300

    feat: canceled schedules function count added

--- src/components/Cards/SchedulesCanceled/index.tsx
+++ src/components/Cards/SchedulesCanceled/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import * as S from './style'
 import { FaUserTimes } from 'react-icons/fa'
+import { renderScheduleList } from '@/services';
+import { getSchedulesMonthCanceledQuantity } from '@/connections';
 
 const SchedulesCanceled = () => {
+    const [schedulesCanceledQuantity, setSchedulesCanceledQuantity] = React.useState<number>(0);
+    React.useEffect(() => {
+        async function loadSchedules() {
+            const { response } = await renderScheduleList();
+            const currentMonthSchedulesCanceled = await getSchedulesMonthCanceledQuantity(response)
+            setSchedulesCanceledQuantity(currentMonthSchedulesCanceled);
+        }
+        loadSchedules();
+    }, []);
     return (
         <S.Container>
             <S.Content>
                 <FaUserTimes />
-                <S.Count>5</S.Count>
+                <S.Count>{schedulesCanceledQuantity}</S.Count>
             </S.Content>
             <S.Title>Cancelamentos do mês</S.Title>
         </S.Container>
