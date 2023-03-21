389c11b84c004a9e8d38f41ca32475639214fe03
Will <wlymes@gmail.com>
2023-03-21 13:47:25 -0300

    feat: profile modal created

---
+++ src/components/Modals/ProfileModal/index.tsx
@@ -0,0 +1,28 @@
+import * as S from './style'
+import React from 'react'
+import { BiLogOut } from 'react-icons/bi'
+import { AuthContext } from '@/components/contexts/authContext'
+
+const ProfileModal = () => {
+    const { user, signOut } = React.useContext(AuthContext)
+    return (
+        <S.Container className='animation-container'>
+            <S.Content>
+                <S.Section>
+                    <S.UserLogo
+                        width={30}
+                        height={30}
+                        alt='user'
+                        src={`https://avatars.dicebear.com/api/initials/${user?.email}.svg`} />
+                    <S.Text>{user?.username}</S.Text>
+                </S.Section>
+                <S.Section>
+                    <S.Text>{user?.email}</S.Text>
+                    <BiLogOut onClick={() => signOut()} />
+                </S.Section>
+            </S.Content>
+        </S.Container>
+    )
+}
+
+export default ProfileModal;
---
+++ src/components/Modals/ProfileModal/style.ts
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+import Image from "next/image";
+
+export const Container = styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 3rem;
+    right: 4rem;
+    width: 15rem;
+    height: 5rem;
+    z-index: 1;
+    border-radius: .4rem;
+    background: rgba(0, 0, 0, .8);
+
+    @media (max-width: 70rem) {
+        right: .5rem;
+    }
+`
+
+export const Content = styled.div`
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    height: 100%;
+    width: 100%;
+    border-radius: .4rem;
+    backdrop-filter: blur(20px);
+    background-color: rgba(51, 42, 42, 0.064);
+`
+
+export const Section = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 5rem;
+    padding: 0 .8rem;
+
+    svg{
+        width: 1.5rem;
+        height: 1.5rem;
+        color: #fff;
+        transition: all .3s;
+
+        &:hover{
+            cursor: pointer;
+            color: #ff6b00;
+        }
+    }
+`
+
+export const UserLogo = styled(Image)`
+    color: #fff;
+    margin-top: .6rem;
+    border-radius: 50%;
+    object-fit: cover;
+
+    &:hover{
+        opacity: .9;
+    }
+`
+export const Text = styled.p`
+    font-size: .8rem;
+    color: #fff;
+    font-weight: bold;
+    text-align: center;
+`
