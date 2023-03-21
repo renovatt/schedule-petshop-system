cd1eaf17a2ce140c909ed7d1b9893d5463a89476
Will <wlymes@gmail.com>
2023-03-21 13:48:03 -0300

    feat: profile modal added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,39 @@
 import React from 'react'
 import * as S from './style'
 import { HeaderProps } from './types'
-import { FaUserCircle } from 'react-icons/fa'
 import Navigation from '../Navigation'
+import ProfileModal from '../Modals/ProfileModal'
+import { AuthContext } from '../contexts/authContext'
+import { FaUserCircle } from 'react-icons/fa'
 
 const Header = ({ title }: HeaderProps) => {
+    const { user } = React.useContext(AuthContext)
+    const [isProfileOpen, setProfileOpen] = React.useState(false)
+    
+    function handleLoad(event: React.SyntheticEvent<HTMLImageElement>): void {
+        const target = event.target as HTMLImageElement;
+        target.style.opacity = "1";
+    }
     return (
-        <S.Header>
-            <Navigation />
-            <S.Title className='animation-container'>{title}</S.Title>
-            <S.Icon>
-                <FaUserCircle />
-            </S.Icon>
-        </S.Header>
+        <>
+            {isProfileOpen && <ProfileModal />}
+            <S.Header>
+                <Navigation />
+                <S.Title className='animation-container'>{title}</S.Title>
+                <S.Icon>
+                    {user?.email ? (
+                        <S.UserLogo
+                            onLoad={handleLoad}
+                            width={35}
+                            height={35}
+                            alt='user'
+                            src={`https://avatars.dicebear.com/api/initials/${user?.email}.svg`}
+                            onClick={() => setProfileOpen(!isProfileOpen)} />
+                    ) : <FaUserCircle />}
+
+                </S.Icon>
+            </S.Header>
+        </>
     )
 }
 
