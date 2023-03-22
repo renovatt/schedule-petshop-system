5aa20108034c3434748481521716baa43d1bcf5e
Will <wlymes@gmail.com>
2023-03-22 14:16:42 -0300

    feat: server side function added

--- src/pages/register-account/index.tsx
+++ src/pages/register-account/index.tsx
@@ -1,6 +1,23 @@
 import Register from '@/views/register'
+import { GetServerSideProps } from 'next'
+import { parseCookies } from 'nookies'
 import React from 'react'
 
 export default function RegisterAccount() {
-    return <Register />
+  return <Register />
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { ['@nextauth-token']: token } = parseCookies(ctx)
+  if (token) {
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false,
+      }
+    }
+  }
+  return {
+    props: {}
+  }
 }
