1f4e92c1ba9ed63d1a1b98a48dba3b942cf5da5c
Will <wlymes@gmail.com>
2023-03-30 20:28:32 -0300

    feat: new hostname added

--- next.config.js
+++ next.config.js
@@ -20,6 +20,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "http.cat",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
   reactStrictMode: true,
