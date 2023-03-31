93c47c8dfb853f1b79849bbcd5cc813a5fc7bd35
Will <wlymes@gmail.com>
2023-03-31 13:02:05 -0300

    chore: meta tags relocated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,14 +14,6 @@ export default function Home() {
     <>
       <Head>
         <title>Login | Cute Pet</title>
-        <meta charSet="utf-8" />
-        <meta name="author" content="Wildemberg Renovato de Lima" />
-        <meta name="description" content="Cute Pet - Sistema de Agendamentos para PetShop" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="keywords" content="petshop, sistema para petshop, Pet Shop" />
-        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
       </Head>
       <main>
         <Login />
